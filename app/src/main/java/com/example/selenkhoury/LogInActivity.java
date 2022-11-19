21fe9118d1e810fc8cecee590c85db282a48a39a
awatefkh3 <awatefkh3@gmail.com>
2022-11-20 00:27:23 +0200

    aa

--- app/src/main/java/com/example/selenkhoury/LogInActivity.java
+++ app/src/main/java/com/example/selenkhoury/LogInActivity.java
@@ -15,15 +15,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LogInActivity extends AppCompatActivity {
 
-    private EditText editTextTextPersonName_login, editTextNumberPassword_login,editTextEmailAddress,editTextPassword,editTextPersonName;
+    private EditText editTextTextPersonName_login, editTextNumberPassword_login,editTextPassword,editTextPersonName;
     private TextView textView2, textview3;
     private Button buttonLogIn, button_to_sign;
-    private final String valid_mail = "admin";
+    private final String valid_Name = "admin";
     private final String valid_password = "1";
 
     SharedPreferences preferences;
-
-    Button buttonLogin, buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +46,10 @@ public class LogInActivity extends AppCompatActivity {
             Toast.makeText(this,"Empty Password",Toast.LENGTH_LONG).show();
         else {
             Intent i = new Intent(this, HomeActivity.class);
-            startActivity(i);
-        }
-
-   /* public void login( ) {
+            startActivity(i);}
+        //
         String input_User = editTextTextPersonName_login.getText().toString();
-        String input_User_pass = editTextNumberPassword_login.getText().toString();
+        String input_User_password = editTextNumberPassword_login.getText().toString();
         if (input_User.length() > 0) {
             Toast.makeText(this, "user logged in !", Toast.LENGTH_LONG).show();
             Intent intent_main = new Intent(this, MainActivity.class);
@@ -63,33 +59,33 @@ public class LogInActivity extends AppCompatActivity {
         }
         Intent i_register = new Intent(this, RegisterActivity.class);
         startActivity(i_register);
-    }*/
+    }
 
         // this line gets the registered email and password , in case no user was registered empty string is returned
-        String input_mail =editTextEmailAddress.getText().toString();
+        String input_Name =editTextPersonName.getText().toString();
         String input_password = editTextPassword.getText().toString();
 
-        String registeredMail = preferences.getString("email","");
+        String registeredName = preferences.getString("Name","");
         String registeredPassword = preferences.getString("password","");
-
-        if (input_mail.equals(registeredMail) && input_password.equals(registeredPassword)){
-            Intent i_mail = new Intent(this,HomeActivity.class);
-            startActivity(i_mail);}
+/*
+        if (input_Name.equals(registeredName) && input_password.equals(registeredPassword)){
+            Intent i_Name = new Intent(this,HomeActivity.class);
+            startActivity(i_Name);}
         else {
             Toast.makeText(this,"ERROR! Email or Password not correct",Toast.LENGTH_SHORT).show();
         }
-    }
+*/
 
 
     @SuppressLint("CommitPrefEdits")
         public void Register(View view) {
 
-            String registeredMail =editTextEmailAddress.getText().toString();
+            String registeredName =editTextPersonName.getText().toString();
             String registeredPassword = editTextPassword.getText().toString();
 
             // saves the email value in the preferences and commits the file
 
-            preferences.edit().putString("email", registeredMail).commit();
+            preferences.edit().putString("User Name", registeredName).commit();
             preferences.edit().putString("password ",registeredPassword ).commit();
 
             Intent i_register = new Intent(this, RegisterActivity.class);
