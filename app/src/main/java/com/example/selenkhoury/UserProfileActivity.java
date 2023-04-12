67b4faba5d941d6cd1d42798e133385453ad1e43
awatefkh3 <awatefkh3@gmail.com>
2023-04-12 23:34:02 +0300

    aa

--- app/src/main/java/com/example/selenkhoury/UserProfileActivity.java
+++ app/src/main/java/com/example/selenkhoury/UserProfileActivity.java
@@ -6,20 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     ImageView cpp ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
     }
 /////// تزبيييططططط
-   /* public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public void changepicprofile (@NonNull ImageView imageView) {
+        switch (imageView.getId()) {
             case R.id.cpp: {
                 Toast.makeText(UserProfileActivity.this, "Camera clicked",Toast.LENGTH_SHORT).show();
                 Intent x = new Intent(this, CameraActivity.class);
@@ -27,17 +29,6 @@ public class UserProfileActivity extends AppCompatActivity {
                 break;
             }
         }
-        return true;
-    }*/
+    }
 
-    public void changepp (@NonNull ImageView Imageview) {
-        switch (ImageView.getBaseline()) {
-            case R.id.cpp: {
-                Toast.makeText(UserProfileActivity.this, "Camera clicked", Toast.LENGTH_SHORT).show();
-                Intent x = new Intent(this, CameraActivity.class);
-                startActivity(x);
-                break;
-            }
-        }
-
-    }}
+}
